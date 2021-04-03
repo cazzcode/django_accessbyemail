6d981d2e974dbc1ad235019b9bf88f3e0c8cd5e2
cazzcode <cazze.sokka@gmail.com>
2021-04-03 15:51:30 -0500

    Release 1.0.1

--- django_accessbyemail/urls.py
+++ django_accessbyemail/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from users.views import LoginView
 from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(), name='login'),
-
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('home/', views.home, name='home'),
 ]
